93be9d38daa3ef5dd1f1ed131bbece936c3de6b6
Aleix Escañuela <aleescfre@alu.edu.gva.es>
2025-04-07 17:27:11 +0200

    Revert "[CHANGE] success en rojo"
    This reverts commit 1e1b801ba59fa42db293978134c4905f64efda7c.

--- resources/views/components/success-message.blade.php
+++ resources/views/components/success-message.blade.php
@@ -1,6 +1,6 @@
 @if(session('successMessage'))
     <div {{ $attributes->merge(['class' => 'flex justify-center']) }}>
-        <div class="min-w-[100px] bg-red-500 text-white rounded px-4 py-2 shadow">
+        <div class="min-w-[100px] bg-green-500 text-white rounded px-4 py-2 shadow">
             {{ session('successMessage') }}
         </div>
     </div>
