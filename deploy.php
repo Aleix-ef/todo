1e1b801ba59fa42db293978134c4905f64efda7c
Aleix Escañuela <aleescfre@alu.edu.gva.es>
2025-04-07 16:48:27 +0200

    [CHANGE] success en rojo

--- deploy.php
+++ deploy.php
@@ -1,31 +1,23 @@
 <?php
+
 namespace Deployer;
 
 require 'recipe/laravel.php';
 
 // Config
 
-set('repository', 'https://github.com/acoloma-edu/todo.git');
+set('repository', 'git@github.com:Aleix-ef/todo.git');
 
 add('shared_files', []);
 add('shared_dirs', []);
 add('writable_dirs', []);
 
 // Hosts
+host('4.212.15.106')->set('remote_user', 'user-deploy')
+                    ->set('identity_file', '~/.ssh/id_rsa')
+                    ->set('deploy_path', '/var/www/user-deploy/html');
 
-host('192.168.1.137')
-    ->set('remote_user', 'ddaw-ud4-deployer')
-    ->set('deploy_path', '/var/www/ddaw-ud4-a4/html');
 
 // Hooks
+
 after('deploy:failed', 'deploy:unlock');
-
-task('build', function () {
-    run('cd {{release_path}} && build');
-});
-
-task('reiniciar-fpm', function () {
-    run('sudo /etc/init.d/php8.1-fpm restart');
-});
-
-after('deploy', 'reiniciar-fpm');
